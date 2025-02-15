5e82f19a6a0f09477564ad69b5f14c8bf43437e2
kamaraj1226 <kamaraj1226@gmail.com>
2025-02-15 12:36:30 +0000

    code refactorization

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,30 @@ use langchain_rust::chain::{
     Chain, ConversationalRetrieverChain, ConversationalRetrieverChainBuilder,
 };
 use langchain_rust::embedding::ollama::ollama_embedder::OllamaEmbedder;
-// use langchain_rust::language_models::llm::LLM;
-// use langchain_rust::language_models::LLMError;
 use langchain_rust::llm::ollama::client::Ollama;
 use langchain_rust::memory::SimpleMemory;
 use langchain_rust::prompt::{HumanMessagePromptTemplate, MessageFormatterStruct};
 use langchain_rust::schemas::messages::Message;
-use langchain_rust::schemas::{Document, StreamData};
+use langchain_rust::schemas::{BaseMemory, Document, StreamData};
 use langchain_rust::vectorstore::qdrant::{Qdrant, Store, StoreBuilder};
 use langchain_rust::{fmt_message, fmt_template, message_formatter, prompt_args, template_jinja2};
+use tokio::sync::Mutex;
+
 use std::io::Write;
 use std::pin::Pin;
+use std::sync::Arc;
 use std::vec;
 use tokio_stream::StreamExt;
 
 use langchain_rust::vectorstore::{Retriever, VecStoreOptions, VectorStore};
 
-const LLM_MODEL: &str = "deepseek-r1:1.5b";
-// const LLM_MODEL: &str = "llama3.2:1b";
+// const LLM_MODEL: &str = "deepseek-r1:1.5b";
+const LLM_MODEL: &str = "llama3.2:1b";
 
 const EMBEDDING_MODEL: &str = "nomic-embed-text";
 const CONNECTION_URL: &str = "http://localhost:6334";
 
-// 1536 dimensions, not 768
-
 async fn add_document(store: &Store, documents: &Vec<Document>) {
-    // let _ = add_documents!(store, documents).await.map_err(|e| {
-    //     eprintln!("Error adding documents: {:?}", e);
-    // });
     let vectorstore_options = VecStoreOptions::default();
     store
         .add_documents(&documents, &vectorstore_options)
@@ -96,7 +92,7 @@ fn get_prompt() -> MessageFormatterStruct {
 while answering you don't need to explictly mention the context, just answer the question in a natural way. If you are asked of the same question please answer that polietly.
 Never makeup the answer.But you can always have a nice conversation with the user."
         )),
-        
+
         fmt_template!(HumanMessagePromptTemplate::new(template_jinja2!("
 Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 If there is no context but the question is general,answer that nicely.
@@ -109,27 +105,19 @@ Helpful Answer:
     ]
 }
 
-async fn get_chain(collection_name: &String) -> ConversationalRetrieverChain {
-    let store: Store = get_store(&collection_name).await;
-    let prompt = get_prompt();
-
-    let model: Ollama = Ollama::default().with_model(LLM_MODEL);
-
-    ConversationalRetrieverChainBuilder::new()
-        .llm(model)
-        .rephrase_question(true)
-        .memory(SimpleMemory::new().into())
-        .retriever(Retriever::new(store, 5))
-        .prompt(prompt)
-        .build()
-        .expect("error in chain")
+fn get_memory() -> Arc<Mutex<SimpleMemory>> {
+    Arc::new(Mutex::new(SimpleMemory::new()))
 }
 
-async fn print_stream(
-    stream: &mut Pin<
-        Box<dyn Stream<Item = Result<StreamData, langchain_rust::chain::ChainError>> + Send>,
-    >,
-) {
+fn get_retriever(store: Store) -> Retriever {
+    let num_docs = 5;
+    Retriever::new(store, num_docs)
+}
+
+type StreamType =
+    Pin<Box<dyn Stream<Item = Result<StreamData, langchain_rust::chain::ChainError>> + Send>>;
+
+async fn print_stream(stream: &mut StreamType) {
     while let Some(res) = stream.next().await {
         match res {
             Ok(data) => {
@@ -153,15 +141,38 @@ fn print_ai_promt() {
     std::io::stdout().flush().unwrap();
 }
 
-async fn cli_chat(collection_name: &String) {
-    let chain = get_chain(collection_name).await;
+async fn get_chain(args: ChatChainArgs) -> ConversationalRetrieverChain {
+    let model = args.model;
+    let prompt = args.prompt;
+    let memory = args.memory;
+    let retriever = args.retriever;
+
+    ConversationalRetrieverChainBuilder::new()
+        .llm(model)
+        .rephrase_question(true)
+        .memory(memory)
+        .retriever(retriever)
+        .prompt(prompt)
+        .build()
+        .expect("error in chain")
+}
+
+async fn cli_chat(args: ChatChainArgs) {
+    let memory = args.memory.clone();
+    let chain = get_chain(args).await;
+    // chain.memory
     let mut query: String;
 
     loop {
         print_user_prompt();
         query = get_query();
+        println!("query: {}", query);
         if query.trim() == String::from("exit") {
             break;
+        }
+
+        if query.trim() == String::from("clear") {
+            memory.lock().await.clear();
         }
 
         if query.trim().is_empty() {
@@ -173,18 +184,25 @@ async fn cli_chat(collection_name: &String) {
             "question" => query
         };
 
-        let mut stream: Pin<
-            Box<dyn Stream<Item = Result<StreamData, langchain_rust::chain::ChainError>> + Send>,
-        > = chain.stream(input_variables).await.unwrap();
+        let mut stream: StreamType = chain.stream(input_variables).await.unwrap();
 
         print_stream(&mut stream).await;
     }
+}
+
+struct ChatChainArgs {
+    prompt: MessageFormatterStruct,
+    model: Ollama,
+    memory: Arc<Mutex<SimpleMemory>>,
+    retriever: Retriever,
 }
 
 #[tokio::main]
 async fn main() {
     // let model_name: String = String::from("deepseek-r1:1.5b");
     println!("=============starting ollama ==================");
+    let model: Ollama = Ollama::default().with_model(LLM_MODEL);
+
     let base_init_required: bool = false;
     let collection_name: String = String::from("rag_test_store");
 
@@ -192,5 +210,11 @@ async fn main() {
         base_init(&collection_name).await;
     }
 
-    cli_chat(&collection_name).await;
+    let args = ChatChainArgs {
+        prompt: get_prompt(),
+        model,
+        memory: get_memory(),
+        retriever: get_retriever(get_store(&collection_name).await),
+    };
+    cli_chat(args).await;
 }
